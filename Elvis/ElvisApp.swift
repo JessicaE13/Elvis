5930899351e715cc4d4b9618cc2fd45528b9e1cc
Jessica Estes <jessica.m.estes@gmail.com>
2025-03-20 18:48:14 -0400

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Jessica Estes on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
